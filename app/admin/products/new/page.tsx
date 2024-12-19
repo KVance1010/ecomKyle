6aeb54cf1f6a181b27b7821e3f50be3c755529d0
KYLE VANCE <vanceofalifetime@protonmail.com>
2024-12-18 17:43:27 -0800

    finished logic of dashboard and products

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PageHeader from "../../_components/PageHeader";
-import  {ProductForm}  from "../_components/ProductForm";
+import { ProductForm } from "../_components/ProductForm";
 
 const page = () => {
     return (
